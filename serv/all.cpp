9432c0d07eb7f1f852eebdc434079cb6c9d73451
Albert Safin <xzfcpw@gmail.com>
2017-08-28 21:06:46 +0700

    Rewrite all.cpp

--- serv/all.cpp
+++ serv/all.cpp
@@ -1,360 +1,413 @@
 #include "all.hpp"
 #include "yearsdiff.hpp"
 #include <cstring>
-#include <boost/intrusive/avl_set.hpp>
 #include <iostream>
+#include <algorithm>
+#include <cassert>
+#include "algorithm.hpp"
 
-namespace bi = ::boost::intrusive;
+#define INVALID_ID      0xFFFFFFFFU
+#define USERS_COUNT         1010000
+#define LOCATIONS_COUNT     1002000
+#define VISIT_COUNT        10010000
 
-struct UserP : User {
-	UserP(const User &user) : User(user) { }
-	bi::avl_set_member_hook<> hook_id;
+struct LoctVist {
+	time_t   visited;
+	uint32_t id;
+	uint32_t user : 24;
+	uint32_t mark :  8;
+
+	LoctVist(const Vist &o)
+		: visited(o.visited)
+		, id     (o.id)
+		, user   (o.user)
+		, mark   (o.mark)
+	{ }
 };
 
-struct LoctP : Loct {
-	LoctP(const Loct &loct) : Loct(loct) {}
-	bi::avl_set_member_hook<> hook_id;
+struct UserVist {
+	time_t visited;
+	uint32_t id;
+	uint32_t loct : 24;
+	uint32_t mark     : 8;
+
+	UserVist(const Vist &o)
+		: visited (o.visited)
+		, id      (o.id)
+		, loct(o.loct)
+		, mark    (o.mark)
+	{ }
 };
 
-struct VistP : Vist {
-	VistP(const Vist &vist) : Vist(vist) {}
-	bi::avl_set_member_hook<> hook_id, hook_loct, hook_user_visted;
-
-	LoctP *loct_ptr = 0;
-	LoctP *get_loct();
-
-	UserP *user_ptr = 0;
-	UserP *get_user();
-};
-
-struct KeyId {
-	typedef uint32_t type;
-	template <class T>
-	uint32_t operator()(const T &a) const {
-		return a.id;
+struct CmpVisited {
+	template <class T> bool operator()(time_t visited, const T &x) const {
+		return visited < x.visited;
+	}
+	template <class T> bool operator()(const T &x, time_t visited) const {
+		return x.visited < visited;
+	}
+	template <class T> bool operator()(const T &x, const T &y) const {
+		return x.visited < y.visited;
 	}
 };
 
-struct CompareVistUserVisted {
-	typedef Vist type;
-	const Vist &operator()(const VistP &v) const {
-		return v;
+template <class T, size_t N>
+struct Array {
+	T data[N];
+	T invalid;
+
+	Array() {
+		for (auto &x : data)
+			x.id = INVALID_ID;
+		invalid.id = INVALID_ID;
 	}
-	bool operator()(const Vist &a, const Vist &b) const {
-		if (a.user < b.user) return true;
-		if (a.user > b.user) return false;
 
-		if (a.visited < b.visited) return true;
-		if (a.visited > b.visited) return false;
-
-		if (a.id < b.id) return true;
-		if (a.id > b.id) return false;
-
-		return false;
+	T &operator[](uint32_t id) {
+		if (id < N)
+			return data[id];
+		std::cout << "INVALID" << id << "\n";
+		abort();
+		return invalid;
 	}
-};
 
-struct CompareVistLoct {
-	typedef Vist type;
-	const Vist &operator()(const VistP &v) const {
-		return v;
+	bool have(uint32_t id) {
+		return id < N && data[id].id != INVALID_ID;
 	}
-	bool operator()(const Vist &a, const Vist &b) const {
-		if (a.loct < b.loct) return true;
-		if (a.loct > b.loct) return false;
 
-		if (a.visited < b.visited) return true;
-		if (a.visited > b.visited) return false;
-
-		if (a.id < b.id) return true;
-		if (a.id > b.id) return false;
-
-		return false;
+	T *get(uint32_t id) {
+		if (id < N && data[id].id != INVALID_ID)
+			return &data[id];
+		return nullptr;
 	}
 };
 
-typedef bi::avl_set<
-	UserP,
-	bi::member_hook<UserP, bi::avl_set_member_hook<>, &UserP::hook_id>,
-	bi::key_of_value<KeyId>
-> UserTreeId;
+template <class T, size_t N>
+struct Array2 {
+	T data[N];
+	T invalid;
 
-typedef bi::avl_set<
-	LoctP,
-	bi::member_hook<LoctP, bi::avl_set_member_hook<>, &LoctP::hook_id>,
-	bi::key_of_value<KeyId>
-> LoctTreeId;
+	T &operator[](uint32_t id) {
+		if (id < N)
+			return data[id];
+		std::cout << "INVALID" << id << "\n";
+		abort();
+		return invalid;
+	}
 
-typedef bi::avl_set<
-	VistP,
-	bi::member_hook<VistP, bi::avl_set_member_hook<>, &VistP::hook_id>,
-	bi::key_of_value<KeyId>
-> VistTreeId;
+	bool have(uint32_t id) {
+		return id < N;
+	}
 
-typedef bi::avl_set<
-	VistP,
-	bi::member_hook<VistP, bi::avl_set_member_hook<>, &VistP::hook_loct>,
-	bi::key_of_value<CompareVistLoct>,
-	bi::compare<CompareVistLoct>
-> VistTreeLoct;
-
-typedef bi::avl_set<
-	VistP,
-	bi::member_hook<VistP, bi::avl_set_member_hook<>, &VistP::hook_user_visted>,
-	bi::key_of_value<CompareVistUserVisted>,
-	bi::compare<CompareVistUserVisted>
-> VistTreeUserVisted;
+	T *get(uint32_t id) {
+		if (id < N)
+			return &data[id];
+		return nullptr;
+	}
+};
 
 struct AllP {
-	UserTreeId            tree_user_id;
-	LoctTreeId        tree_loct_id;
-	VistTreeId           tree_vist_id;
+	Array<User, USERS_COUNT>     users;
+	Array<Loct, LOCATIONS_COUNT> locts;
+	Array<Vist, VISIT_COUNT>     vists;
 
-	VistTreeLoct     tree_vist_loct;
-	VistTreeUserVisted  tree_vist_user_visted;
+	Array2<std::vector<UserVist>, USERS_COUNT> user_visits;
+	Array2<std::vector<LoctVist>, LOCATIONS_COUNT> loct_visits;
 
 	YearDiffer year_differ;
 };
 
-static AllP allp;
+static AllP &all = *(new AllP());
 
-
-LoctP *VistP::get_loct() {
-	if (loct_ptr != nullptr)
-		return loct_ptr;
-	auto f = allp.tree_loct_id.find(loct);
-	loct_ptr = f != allp.tree_loct_id.end() ? &*f : nullptr;
-	if (loct_ptr == nullptr)
-		std::cout << "Wooo" << std::endl;
-	return loct_ptr;
+template <class T>
+typename std::vector<T>::iterator
+find_by_visited_and_id(std::vector<T> &vec, time_t visited_at, uint32_t id) {
+    auto range = std::equal_range(vec.begin(), vec.end(), visited_at, CmpVisited{});
+    auto fun = [id](const T &item) -> bool { return item.id == id; };
+    auto it = std::find_if(range.first, range.second, fun);
+    assert(it != vec.end());
+    assert(it->id == id);
+    assert(it->visited == visited_at);
+    return it;
 }
 
-UserP *VistP::get_user() {
-	if (user_ptr != nullptr)
-		return user_ptr;
-	auto f = allp.tree_user_id.find(user);
-	user_ptr = f != allp.tree_user_id.end() ? &*f : nullptr;
-	if (user_ptr == nullptr)
-		std::cout << "Hooo" << std::endl;
-	return user_ptr;
+template <class T>
+void
+erase_by_visited_and_id(std::vector<T> &vec, time_t visited_at, uint32_t id) {
+	vec.erase(find_by_visited_and_id(vec, visited_at, id));
 }
 
 
 bool All::add(const User &user) {
-	auto it = allp.tree_user_id.find(user.id);
-	if (it != allp.tree_user_id.end())
+	if (all.users[user.id].id != INVALID_ID)
 		return false;
-	auto e = new UserP(user);
-	allp.tree_user_id.insert(it, *e);
+	all.users[user.id] = user;
 	return true;
-}
+};
 
 bool All::add(const Loct &loct) {
-	auto it = allp.tree_loct_id.find(loct.id);
-	if (it != allp.tree_loct_id.end())
+	if (all.locts[loct.id].id != INVALID_ID)
 		return false;
-	auto e = new LoctP(loct);
-	allp.tree_loct_id.insert(it, *e);
+	all.locts[loct.id] = loct;
 	return true;
-}
+};
 
 bool All::add(const Vist &vist) {
-	auto it = allp.tree_vist_id.find(vist.id);
-	if (it != allp.tree_vist_id.end())
+	if (all.vists[vist.id].id != INVALID_ID)
 		return false;
-	auto e = new VistP(vist);
-	allp.tree_vist_id.insert(it, *e);
-	allp.tree_vist_loct.insert(*e);
-	allp.tree_vist_user_visted.insert(*e);
+	insert_sorted(all.user_visits[vist.user], {vist}, CmpVisited{});
+	insert_sorted(all.loct_visits[vist.loct], {vist}, CmpVisited{});
+	all.vists[vist.id] = vist;
 	return true;
-}
-
-bool All::update(const User &user, UserMask mask) {
-	auto it = allp.tree_user_id.find(user.id);
-	if (it == allp.tree_user_id.end())
-		return false;
-	if (mask.email)          std::strcpy(it->email,           user.email);
-	if (mask.first_name)     std::strcpy(it->first_name,      user.first_name);
-	if (mask.last_name)      std::strcpy(it->last_name,       user.last_name);
-	if (mask.gender_is_male) it->gender_is_male = user.gender_is_male;
-	if (mask.birth_date)     it->birth_date     = user.birth_date;
-	return true;
-}
-
-bool All::update(const Loct &loct, LoctMask mask) {
-	auto it = allp.tree_loct_id.find(loct.id);
-	if (it == allp.tree_loct_id.end())
-		return false;
-	if (mask.place)    it->place    = loct.place;
-	if (mask.country)  std::strcpy(it->country,   loct.country);
-	if (mask.city)     std::strcpy(it->city,      loct.city);
-	if (mask.distance) it->distance = loct.distance;
-	return true;
-}
-
-bool All::update(const Vist &vist, VistMask mask) {
-	auto it = allp.tree_vist_id.find(vist.id);
-	if (it == allp.tree_vist_id.end())
-		return false;
-
-	if (mask.loct || mask.visited)
-		allp.tree_vist_loct.erase(*it);
-
-	if (mask.user || mask.visited)
-		allp.tree_vist_user_visted.erase(*it);
-
-	if (mask.loct)      { it->loct      = vist.loct; it->loct_ptr = 0; }
-	if (mask.user)      { it->user      = vist.user; it->user_ptr = 0; }
-	if (mask.visited)   it->visited = vist.visited;
-	if (mask.mark)        it->mark      = vist.mark;
-
-	if (mask.loct || mask.visited)
-		allp.tree_vist_loct.insert(*it);
-
-	if (mask.user || mask.visited)
-		allp.tree_vist_user_visted.insert(*it);
-
-	return true;
-}
+};
 
 User *All::get_user(uint32_t id) {
-	auto f = allp.tree_user_id.find(id);
-	return f != allp.tree_user_id.end() ? &*f : nullptr;
+	return all.users.get(id);
 }
 
 Loct *All::get_loct(uint32_t id) {
-	auto f = allp.tree_loct_id.find(id);
-	return f != allp.tree_loct_id.end() ? &*f : nullptr;
+	return all.locts.get(id);
 }
 
 Vist *All::get_vist(uint32_t id) {
-	auto f = allp.tree_vist_id.find(id);
-	return f != allp.tree_vist_id.end() ? &*f : nullptr;
+	return all.vists.get(id);
 }
 
-bool All::get_vists(
-	std::vector<VistData> &out,
-	uint32_t id,
-	boost::optional<time_t> from_date,
-	boost::optional<time_t> to_date,
-	boost::optional<std::string> country,
-	boost::optional<uint32_t> to_distance
-)
+#define UPDATE(X)   if (mask.X) curr->X = upd.X
+#define UPDATE_S(X) if (mask.X) strcpy(curr->X, upd.X)
+
+bool All::update(const User &upd, UserMask mask) {
+	auto curr = all.users.get(upd.id);
+	if (curr == nullptr)
+		return false;
+	UPDATE_S(email);
+	UPDATE_S(first_name);
+	UPDATE_S(last_name);
+	UPDATE  (gender_is_male);
+	UPDATE  (birth_date);
+	return true;
+}
+
+bool All::update(const Loct &upd, LoctMask mask) {
+	auto curr = all.locts.get(upd.id);
+	if (curr == nullptr)
+		return false;
+	UPDATE  (place);
+	UPDATE_S(country);
+	UPDATE_S(city);
+	UPDATE  (distance);
+	return true;
+}
+
+#undef UPDATE_S
+
+static void debug03_check(uint32_t vist_id, uint32_t loct_id, uint32_t user_id) {
+#ifdef DEBUG_03
+	bool loct_found = false, user_found;
+
+	printf("loct %lu = [\n", loct_id);
+	time_t prev_visited;
+
+	prev_visited = -999999;
+	for (auto &vist : all.loct_visits[loct_id]) {
+		printf("  %20ld  %5lu %5lu %5lu\n", vist.visited, vist.id, vist.user, vist.mark);
+		assert(vist.visited > prev_visited); prev_visited = vist.visited;
+		if (vist.id == vist_id) loct_found = true;
+		assert(all.vists.have(vist.id));
+		assert(all.vists[vist.id].user == vist.user);
+	}
+	printf("]\n" "user %lu = [\n", user_id);
+
+	prev_visited = -999999;
+	for (auto &vist : all.user_visits[loct_id]) {
+		printf("  %20ld  %5lu %5lu %5lu\n", vist.visited, vist.id, vist.loct, vist.mark);
+		assert(vist.visited > prev_visited); prev_visited = vist.visited;
+		if (vist.id == vist_id) user_found = true;
+		assert(all.vists.have(vist.id));
+		assert(all.vists[vist.id].loct == vist.loct);
+	}
+
+	assert(loct_found);
+	assert(user_found);
+	printf("]\n\n\n");
+#endif
+}
+
+bool All::update(const Vist &upd, VistMask mask) {
+	auto curr = all.vists.get(upd.id);
+	if (curr == nullptr)
+		return false;
+
+	#define IGNORE(X) if (mask.X && curr->X == upd.X) mask.X = false
+	IGNORE(loct);
+	IGNORE(user);
+	IGNORE(visited);
+	IGNORE(mark);
+	#undef IGNORE
+
+	debug03_check(curr->id, curr->loct, curr->user);
+
+	if (mask.loct)
+		erase_by_visited_and_id(
+				all.loct_visits[curr->loct],
+				curr->visited, curr->id);
+	if (mask.user)
+		erase_by_visited_and_id(
+				all.user_visits[curr->user],
+				curr->visited, curr->id);
+
+	if (mask.visited) {
+		if (!mask.loct) {
+			auto &vec = all.loct_visits[curr->loct];
+			auto it = find_by_visited_and_id(vec, curr->visited, curr->id);
+			auto new_pos = std::upper_bound(vec.begin(), vec.end(), upd.visited, CmpVisited{});
+			it->visited = upd.visited;
+			if (mask.mark) it->mark = upd.mark;
+			if (mask.user) it->user = upd.user;
+			reorder(it, new_pos);
+		}
+		if (!mask.user) {
+			auto &vec = all.user_visits[curr->user];
+			auto it = find_by_visited_and_id(vec, curr->visited, curr->id);
+			auto new_pos = std::upper_bound(vec.begin(), vec.end(), upd.visited, CmpVisited{});
+
+			it->visited = upd.visited;
+			if (mask.mark) it->mark = upd.mark;
+			if (mask.loct) it->loct = upd.loct;
+
+			reorder(it, new_pos);
+		}
+	} else if (mask.mark || mask.loct || mask.user) {
+		if (!mask.loct) {
+			auto vist = find_by_visited_and_id(
+				all.loct_visits[curr->loct],
+				curr->visited, curr->id);
+			if (mask.mark) vist->mark = upd.mark;
+			if (mask.user) vist->user = upd.user;
+		}
+		if (!mask.user) {
+			auto vist = find_by_visited_and_id(
+				all.user_visits[curr->user],
+				curr->visited, curr->id);
+			if (mask.mark) vist->mark = upd.mark;
+			if (mask.loct) vist->loct = upd.loct;
+		}
+	}
+
+	UPDATE(loct);
+	UPDATE(user);
+	UPDATE(visited);
+	UPDATE(mark);
+
+	if (mask.loct) insert_sorted(all.loct_visits[curr->loct], {*curr}, CmpVisited{});
+	if (mask.user) insert_sorted(all.user_visits[curr->user], {*curr}, CmpVisited{});
+
+	debug03_check(curr->id, curr->loct, curr->user);
+
+	return true;
+}
+
+
+static 
+std::pair<unsigned, unsigned>
+get_avg_(uint32_t loct_id,
+	 time_t   from_date,
+	 time_t   to_date,
+	 time_t   from_birth,
+	 time_t   to_birth,
+	 boost::optional<bool> gender_is_male)
 {
-	out.clear();
-	// if (!allp.tree_user_id.find(id))
-	//	return false;
+	auto visits = all.loct_visits.get(loct_id);
+	if (visits == nullptr)
+		return {0, 0};
 
-	if (!from_date) from_date = { std::numeric_limits<time_t>::min() };
-	if (!to_date)   to_date   = { std::numeric_limits<time_t>::max() };
-
-	if (*from_date >= *to_date)
-		return true;
-
-	Vist first;
-	first.user = id;
-	first.visited = *from_date;
-	first.id = 0;
-
-	Vist last;
-	last.user = id;
-	last.visited = *to_date;
-	last.id = 0xFFFFFFFFu;
-
-	auto it = allp.tree_vist_user_visted.lower_bound(first);
-	if (it == allp.tree_vist_user_visted.end())
-		return true;
-
-	auto end = allp.tree_vist_user_visted.upper_bound(last);
-
-	for (; it != end; it++) {
-		auto &vist = *it;
-		Loct *loct = vist.get_loct();
-		if (loct == nullptr)
+	unsigned res_sum = 0, res_cnt = 0;
+	for (auto visit = std::upper_bound(visits->begin(), visits->end(), from_date, CmpVisited{});
+	     visit != visits->end() && visit->visited < to_date;
+	     visit++)
+	{
+		auto user = all.users[visit->user];
+		if (user.birth_date <= from_birth ||
+		    user.birth_date >= to_birth ||
+		    (gender_is_male && gender_is_male != user.gender_is_male))
 			continue;
 
-		if (country     && loct->country != *country ||
-		    to_distance && loct->distance >= *to_distance)
-			continue;
+		res_sum += visit->mark;
+		res_cnt ++;
+	}
+	return {res_sum, res_cnt};
+}
 
-		out.push_back({
-			vist.mark,
-			vist.visited,
-			loct->place
-		});
+static
+bool
+get_visits_(std::vector<VistData> &out,
+            uint32_t    user_id,
+	    time_t      from_date,
+	    time_t      to_date,
+	    boost::optional<std::string> country,
+	    uint32_t    to_distance)
+{
+	auto visits = all.user_visits.get(user_id);
+	if (visits == nullptr)
+		return false;
+	for (auto visit = std::upper_bound(visits->begin(), visits->end(), from_date, CmpVisited{});
+	     visit != visits->end() && visit->visited < to_date;
+	     visit++)
+	{
+		auto loct = all.locts.get(visit->loct);
+		if (loct == nullptr ||
+		    country && loct->country != *country ||
+		    loct->distance >= to_distance)
+			continue;
+		out.push_back({visit->mark, visit->visited, loct->place});
 	}
 	return true;
 }
 
+bool All::get_vists(
+		std::vector<VistData> &out,
+		uint32_t id,
+		boost::optional<time_t>      from_date,
+		boost::optional<time_t>      to_date,
+		boost::optional<std::string> country,
+		boost::optional<uint32_t>    to_distance
+	      )
+{
+	out.clear();
+	return get_visits_(
+		out, id,
+		from_date ? *from_date : std::numeric_limits<time_t>::min(),
+		to_date   ? *to_date   : std::numeric_limits<time_t>::max(),
+		country,
+		to_distance ? *to_distance : 0xFFFFFFFFU);
+		
+}
+
 double All::get_avg(
-	uint32_t id,
-	boost::optional<time_t>  from_date,
-	boost::optional<time_t>  to_date,
-	boost::optional<uint8_t> from_age,
-	boost::optional<uint8_t> to_age,
-	boost::optional<bool>    gender_is_male
-) {
-	if (!from_date) from_date = { std::numeric_limits<time_t>::min() };
-	if (!to_date)   to_date   = { std::numeric_limits<time_t>::max() };
-
-	time_t birth_from, birth_to;
-	birth_from = to_age
-		? allp.year_differ.jaja(*to_age)
-		: std::numeric_limits<time_t>::min();
-
-	birth_to = from_age
-		? allp.year_differ.jaja(*from_age)
-		: std::numeric_limits<time_t>::max();
-
-	if (*from_date >= *to_date)
+		uint32_t id,
+		boost::optional<time_t>  from_date,
+		boost::optional<time_t>  to_date,
+		boost::optional<uint8_t> from_age,
+		boost::optional<uint8_t> to_age,
+		boost::optional<bool>    gender_is_male
+	      )
+{
+	auto res =  get_avg_(
+		id,
+		from_date ? *from_date : std::numeric_limits<time_t>::min(),
+		to_date   ? *to_date   : std::numeric_limits<time_t>::max(),
+		to_age   ? all.year_differ.jaja(*to_age) : std::numeric_limits<time_t>::min(),
+		from_age ? all.year_differ.jaja(*from_age) : std::numeric_limits<time_t>::max(),
+		gender_is_male);
+	if (res.second == 0)
 		return 0;
-
-	Vist first;
-	first.loct = id;
-	first.visited = *from_date;
-	first.id = 0;
-
-	Vist last;
-	last.loct = id;
-	last.visited = *to_date;
-	last.id = 0xFFFFFFFFu;
-
-	auto it = allp.tree_vist_loct.lower_bound(first);
-	if (it == allp.tree_vist_loct.end())
-		return 0;
-
-	auto end = allp.tree_vist_loct.upper_bound(last);
-
-	unsigned count = 0, sum = 0;
-	for (; it != end; it++) {
-		auto &vist = *it;
-		UserP *user = vist.get_user();
-		if (user == nullptr)
-			continue;
-
-		if (user->birth_date <= birth_from ||
-		    user->birth_date >= birth_to   ||
-		    gender_is_male && user->gender_is_male != *gender_is_male)
-			continue;
-
-		count++;
-		sum += vist.mark;
-	}
-
-	if (count == 0)
-		return 0;
-	return (double)sum / count;
+	return (res.first / (double)res.second) + 1e-7;
 }
 
 void All::optimize() {
-	for (auto &vist : allp.tree_vist_id) {
-		vist.get_loct();
-		vist.get_user();
-	}
 }
 
 void All::set_options(time_t now, bool full) {
-	allp.year_differ.set_now(now);
+	all.year_differ.set_now(now);
 }
 
 namespace All {
